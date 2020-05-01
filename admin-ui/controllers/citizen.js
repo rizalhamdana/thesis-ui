7403f772fc80263ef536ab7ba3afb9a97e1ab3f4
rizalhamdana <ari.gusti12@gmail.com>
2020-05-01 12:10:17 +0700

    adding base url for api gateway

--- admin-ui/controllers/citizen.js
+++ admin-ui/controllers/citizen.js
@@ -1,174 +1,165 @@
-function getAllCitizen(token){
-    const url = "http://192.168.1.3:31679/citizens"
-    const other_params = {
-        method: "GET",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-            "Token": token
-        },
-        mode: "cors"
-      };
-    var result = fetch(url, other_params)
-      .then(function(response) {
-        if (response.ok) {
-        
-          return response.text();
-        } else if(response.status==401) {
-          throw new Error("Unauthorized");
-        } else {
-          throw new Error("Could Not Reach API");
-        }
-      })
-      .then(function(data) {
-            jsonData = JSON.parse(data)
-            return jsonData
-      })
-      .catch(function(error) {
-        console.log(error.message)
-        return error     
-      });
-    return result;
-}
-function getOneCitizen(token, nik){
-  const url = "http://192.168.1.3:31679/citizens/"+nik;
+var apiGatewayURL = "http://192.168.1.6:31679";
+function getAllCitizen(token) {
+  const url = apiGatewayURL + "/citizens";
   const other_params = {
     method: "GET",
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Token": token
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
     },
-    mode: "cors"
+    mode: "cors",
   };
-var result = fetch(url, other_params)
-  .then(function(response) {
-    if (response.ok) {
-    
-      return response.text();
-    } else if(response.status==401) {
-      throw new Error("Unauthorized");
-    } else if(response.status==404){
-      throw new Error("Not Found")
-    } else {
-      throw new Error("Could Not Reach API");
-    }
-  })
-  .then(function(data) {
-        jsonData = JSON.parse(data)
-        return jsonData
-  })
-  .catch(function(error) {
-    console.log(error.message)
-    return error     
-  });
-return result;
-
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      jsonData = JSON.parse(data);
+      return jsonData;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
 }
-function insertCitizen(token, data){
-  const url = "http://192.168.1.3:31679/citizens";
+function getOneCitizen(token, nik) {
+  const url = apiGatewayURL + "/citizens/" + nik;
+  const other_params = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
+    },
+    mode: "cors",
+  };
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else if (response.status == 404) {
+        throw new Error("Not Found");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      jsonData = JSON.parse(data);
+      return jsonData;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
+}
+function insertCitizen(token, data) {
+  const url = apiGatewayURL + "/citizens";
   const other_params = {
     method: "POST",
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Token": token
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   };
-var result = fetch(url, other_params)
-  .then(function(response) {
-    if (response.ok) {
-      return response.text();
-    } else if(response.status==401) {
-      throw new Error("Unauthorized");
-    } else if(response.status==404){
-      throw new Error("Not Found")
-    } else {
-      throw new Error("Could Not Reach API");
-    }
-  })
-  .then(function(data) {
-        console.log(data)
-        return data
-  })
-  .catch(function(error) {
-    console.log(error.message)
-    return error     
-  });
-return result;
-
-
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else if (response.status == 404) {
+        throw new Error("Not Found");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
 }
-function deleteOneCitizen(token, nik){
-  const url = "http://192.168.1.3:31679/citizens/"+nik;
+function deleteOneCitizen(token, nik) {
+  const url = apiGatewayURL + "/citizens/" + nik;
   const other_params = {
     method: "DELETE",
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Token": token
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
     },
-    mode: "cors"
+    mode: "cors",
   };
-var result = fetch(url, other_params)
-  .then(function(response) {
-    if (response.ok) {
-    
-      return response.text();
-    } else if(response.status==401) {
-      throw new Error("Unauthorized");
-    } else if(response.status==404){
-      throw new Error("Not Found")
-    } else {
-      throw new Error("Could Not Reach API");
-    }
-  })
-  .then(function(data) {
-        console.log(data)
-        return data
-  })
-  .catch(function(error) {
-    console.log(error.message)
-    return error     
-  });
-return result;
-
-
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else if (response.status == 404) {
+        throw new Error("Not Found");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
 }
-function updateOneCitizen(token, data, nik){
-  const url = "http://192.168.1.3:31679/citizens/"+nik;
+function updateOneCitizen(token, data, nik) {
+  const url = apiGatewayURL + "/citizens/" + nik;
   const other_params = {
     method: "PUT",
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Token": token
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   };
-var result = fetch(url, other_params)
-  .then(function(response) {
-    if (response.ok) {
-      return response.text();
-    } else if(response.status==401) {
-      throw new Error("Unauthorized");
-    } else if(response.status==404){
-      throw new Error("Not Found")
-    } else {
-      throw new Error("Could Not Reach API");
-    }
-  })
-  .then(function(data) {
-        console.log(data)
-        return data
-  })
-  .catch(function(error) {
-    console.log(error.message)
-    return error     
-  });
-return result;
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else if (response.status == 404) {
+        throw new Error("Not Found");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
 }
-function verifyOneCitizen(){
-
-}
+function verifyOneCitizen() {}
--- admin-ui/controllers/family.js
+++ admin-ui/controllers/family.js
@@ -1,100 +1,99 @@
-function getAllFamilies(token){
-    const url = "http://192.168.1.3:31679/family"
-    const other_params = {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Token": token
-        },
-        mode: "cors"
-      };
-    var result = fetch(url, other_params)
-      .then(function(response) {
-        if (response.ok) {
-          
-          return response.text();
-        } else if(response.status==401) {   
-          throw new Error("Unauthorized");
-        } else {  
-          throw new Error("Could Not Reach API");
-        }
-      })
-      .then(function(data) {
-            jsonData = data == null || data == '' || data == undefined ? [] : JSON.parse(data);
-            
-            return jsonData;
+var apiGatewayURL = "http://192.168.1.6:31679";
+function getAllFamilies(token) {
+  const url = apiGatewayURL + "/family";
+  const other_params = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
+    },
+    mode: "cors",
+  };
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      jsonData =
+        data == null || data == "" || data == undefined ? [] : JSON.parse(data);
 
-      })
-      .catch(function(error) {
-        return error     
-      });
-    return result;
+      return jsonData;
+    })
+    .catch(function (error) {
+      return error;
+    });
+  return result;
 }
 
-function getOneFamily(token, familyCardNumber){
-    const url = "http://192.168.1.3:31679/family/"+familyCardNumber
-    const other_params = {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Token": token
-        },
-        mode: "cors"
-      };
-    var result = fetch(url, other_params)
-      .then(function(response) {
-        if (response.ok) {
-          
-          return response.text();
-        } else if(response.status==401) {   
-          throw new Error("Unauthorized");
-        } else {  
-          throw new Error("Could Not Reach API");
-        }
-      })
-      .then(function(data) {
-            jsonData = data == null || data == '' || data == undefined ? [] : JSON.parse(data);
-            
-            return jsonData;
+function getOneFamily(token, familyCardNumber) {
+  const url = apiGatewayURL + "/family/" + familyCardNumber;
+  const other_params = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
+    },
+    mode: "cors",
+  };
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      jsonData =
+        data == null || data == "" || data == undefined ? [] : JSON.parse(data);
 
-      })
-      .catch(function(error) {
-        return error     
-      });
-    return result;
+      return jsonData;
+    })
+    .catch(function (error) {
+      return error;
+    });
+  return result;
 }
 
-function verifyOneFamily(token, regisNumber){
-    const url = "http://192.168.1.3:31679/family/verify/"+regisNumber;
+function verifyOneFamily(token, regisNumber) {
+  const url = apiGatewayURL + "/verify/family/" + regisNumber;
   const other_params = {
     method: "PUT",
     headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        "Token": token
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Token: token,
     },
   };
-var result = fetch(url, other_params)
-  .then(function(response) {
-    if (response.ok) {
-      return response.text();
-    } else if(response.status==401) {
-      throw new Error("Unauthorized");
-    } else if(response.status==404){
-      throw new Error("Not Found")
-    } else {
-      throw new Error("Could Not Reach API");
-    }
-  })
-  .then(function(data) {
-        console.log(data)
-        return data
-  })
-  .catch(function(error) {
-    console.log(error.message)
-    return error     
-  });
-return result;
+  var result = fetch(url, other_params)
+    .then(function (response) {
+      if (response.ok) {
+        return response.text();
+      } else if (response.status == 401) {
+        throw new Error("Unauthorized");
+      } else if (response.status == 404) {
+        throw new Error("Not Found");
+      } else {
+        throw new Error("Could Not Reach API");
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error.message);
+      return error;
+    });
+  return result;
 }
